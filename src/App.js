efda3d05a29c2e514bcc5e2deecf216f04dd93a1
Rodrigo Hernan Saucedo <rodrigohernansaucedo@gmail.com>
2023-02-05 13:53:07 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hola mundo</p>
+    </div>
+  );
+}
+
+export default App;
