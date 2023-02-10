a89a0e85ba19603db1a79c8b244d9bcd9505992a
Agustin <acaceres107@gmail.com>
2023-02-10 07:25:22 -0300

    maquetado

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,12 @@ import CardDetails from '../components/cardDetails'
 import Home from './Home/Home'
 import React from 'react'
 import {createBrowserRouter} from "react-router-dom"
+import ShoppingCart from '../components/cart/Cart'
 
 const indexRouter = createBrowserRouter([
     { path: "/", element : <Home /> } ,
-    { path: "/details", element : <CardDetails /> } 
+    { path: "/details", element : <CardDetails /> } ,
+    { path: "/shopping-cart", element: <ShoppingCart/> }
 ])
 
 export default indexRouter 
