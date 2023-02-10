d1bcb989a5b2a5b2f2b97ee1adb73fcc533ba0ef
LucianaRovere <luciana.rovere96@gmail.com>
2023-02-10 11:18:14 -0300

    front listo para presentar

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -12,9 +12,13 @@ function Categories() {
 const categories = useSelector(store=>store.categories?.categories)
 
 const dispatch = useDispatch()
+
+let token = localStorage.getItem("token")
  
 useEffect(()=>{
-  dispatch(getCategories())
+  dispatch(getCategories({
+    token
+  }))
 },[])
 
 
