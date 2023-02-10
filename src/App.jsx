a1eef441edc9ca5f2f68a18388536f3b03b0c7d0
PabloMunizMontoya <pablomunizmontoya@gmail.com>
2023-02-10 02:14:24 -0500

    signin y signup funcionando, falta terminar el estilo de signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import { RouterProvider } from "react-router-dom";
 import indexRouter from "./router/index";
 
+import { useEffect } from 'react'
+
+import { useDispatch } from 'react-redux'
+import authActions from "./store/auth/actions"
+const { iniciar_sesion_con_token } = authActions
+
 function App() {
+
+    let dispatch = useDispatch()
+
+    useEffect(() => {
+        let token = localStorage.getItem('token')
+        //console.log(token)
+        if (token) {
+            dispatch(iniciar_sesion_con_token(token))
+        }
+    },[])
     return <RouterProvider router={indexRouter} />;
 }
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
+import {createBrowserRouter} from "react-router-dom"
 import CardDetails from '../components/cardDetails'
 import Home from './Home/Home'
 import React from 'react'
-import {createBrowserRouter} from "react-router-dom"
+import SignIn from './Login/SignIn'
+import SignUp from "./Login/SignUp"
 
 const indexRouter = createBrowserRouter([
     { path: "/", element : <Home /> } ,
-    { path: "/details", element : <CardDetails /> } 
+    { path: "/details", element : <CardDetails /> },
+    { path:"/signin",element:<SignIn/> },
+    { path:"/signup",element:<SignUp/> } 
 ])
 
 export default indexRouter 
