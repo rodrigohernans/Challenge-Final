c3cba948edd5e096f3422615a208fe674bfffdf2
LucianaRovere <luciana.rovere96@gmail.com>
2023-02-15 19:04:15 -0300

    maquetado de favoritos

---
+++ src/components/favorites/CardFavorites.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './favorites.module.css'
+
+function CardFavorites() {
+  return (
+    <div className={styles.container}>
+        <img className={styles.img} src="https://p4.wallpaperbetter.com/wallpaper/424/216/1017/power-chainsaw-man-chainsaw-man-manga-hd-wallpaper-preview.jpg" alt="" />
+
+        <div className={styles.data}> <p className={styles.title}>Titulo del juego</p>
+        <div className={styles.allButtons}><div className={styles.buttons}>
+        <img  className={styles.fav} src="../../assets/favoriteIcon.png" alt="" />
+        <img  className={styles.cart} src="../../assets/shopIcon.png" alt="" />
+        </div></div>
+        
+        
+        <button className={styles.price}>&1234</button>
+    </div>
+       </div>
+  )
+}
+
+export default CardFavorites
--- src/router/Home/Home.jsx
+++ src/router/Home/Home.jsx
@@ -1,13 +1,11 @@
 import 'react-multi-carousel/lib/styles.css';
-
 import React,{ useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-
-import { Link as Anchor } from "react-router-dom";
-import Card from '../../components/card/Card'
-import Carousel from 'react-multi-carousel';
 import Categories from '../../components/categories/Categories'
+import Card from '../../components/card/Card'
+import { useDispatch, useSelector } from "react-redux";
 import allGamesActions from "../../store/allGames/actions";
+import { Link as Anchor } from "react-router-dom";
+import Carousel from 'react-multi-carousel';
 import styles from './home.module.css'
 
 const {getAllGames} = allGamesActions
@@ -95,11 +93,6 @@ useEffect(()=>{
 
        
      </Carousel>
-    
-
-       
-       
-    
             <Categories/>
          <Card/> 
             </div>
