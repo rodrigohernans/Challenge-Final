5cb63e4193f3b65e1822d3ce2244131a4ab6b993
LucianaRovere <luciana.rovere96@gmail.com>
2023-02-16 07:01:46 -0300

    maquetado de favoritos casi terminado

--- src/components/favorites/CardFavorites.jsx
+++ src/components/favorites/CardFavorites.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
-import styles from './favorites.module.css'
+import React from "react";
+import styles from "./favorites.module.css";
 
 function CardFavorites() {
   return (
     <div className={styles.container}>
-        <img className={styles.img} src="https://p4.wallpaperbetter.com/wallpaper/424/216/1017/power-chainsaw-man-chainsaw-man-manga-hd-wallpaper-preview.jpg" alt="" />
+      <section className={styles.card}>
+        <img
+          className={styles.img}
+          src="https://p4.wallpaperbetter.com/wallpaper/424/216/1017/power-chainsaw-man-chainsaw-man-manga-hd-wallpaper-preview.jpg"
+          alt=""
+        />
 
-        <div className={styles.data}> <p className={styles.title}>Titulo del juego</p>
-        <div className={styles.allButtons}><div className={styles.buttons}>
-        <img  className={styles.fav} src="../../assets/favoriteIcon.png" alt="" />
-        <img  className={styles.cart} src="../../assets/shopIcon.png" alt="" />
-        </div></div>
-        
-        
-        <button className={styles.price}>&1234</button>
+        <div className={styles.data}>
+          {" "}
+          <p className={styles.title}>Titulo del juego</p>
+          <div className={styles.allButtons}>
+            <div className={styles.buttons}>
+              <img
+                className={styles.fav}
+                src="../../assets/favoriteIcon.png"
+                alt=""
+              />
+              <img
+                className={styles.cart}
+                src="../../assets/shopIcon.png"
+                alt=""
+              />
+            </div>
+            <button className={styles.price}>$1234</button>
+          </div>
+        </div>
+      </section>
     </div>
-       </div>
-  )
+  );
 }
 
-export default CardFavorites
+export default CardFavorites;
